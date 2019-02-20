5f7d190fb2e1608ea9393dc59adacb1838b57a46
iambb5445 <iambb5445@yahoo.com>
2019-02-20 15:42:56 +0330

    Worker class deleted and its methods and fields moved to User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,11 +1,29 @@
+import java.util.List;
+
 public class User {
     private String username;
 
-    public User(String username) {
+    public String getUsername() { return username; }
+
+    private List<Skill> skills;
+
+    public User (String username, List<Skill> skills) {
         this.username = username;
+        this.skills = skills;
     }
 
-    public String getUsername() {
-        return username;
+    public User(String username, Skill... skills)
+    {
+        this.username = username;
+        for (Skill skill : skills)
+            this.skills.add(skill);
+    }
+
+    public long getSkillPoint(Skill jobSkill) {
+        for(Skill userSkill : skills) {
+            if(userSkill.getName().equals(jobSkill.getName()))
+                return userSkill.getPoints();
+        }
+        return -1;
     }
 }
