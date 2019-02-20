75bf654db17d7e862f29c84d0ce6ab919e5cb6cc
amirhmi <amirhmi1377@gmail.com>
2019-02-20 18:43:37 +0330

    http request and json parser tested

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,16 @@ import java.util.Scanner;
 
 public class Main {
     private static HttpClientGet http_client_get = new HttpClientGet();
+    private static DataBase db = new DataBase();
 
     public static void main(String[] args) {
         try {
             String project_data_json = http_client_get.HttpGetRequest(RequestType.PROJECT);
             String skills_data_json = http_client_get.HttpGetRequest(RequestType.SKILL);
+            List<Project> projs = JsonParser.parseProjectList(project_data_json);
+            for(Project p : projs)
+                DataBase.addProject(p);
+            Skill.setValidNames(JsonParser.parseNameList(skills_data_json));
         }
         catch (IOException ioException) {
             return;
