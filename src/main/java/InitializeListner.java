252aeff85852b4a289dbbcfd9c6cde9398dc6729
amirhmi <amirhmi1377@gmail.com>
2019-02-28 11:57:31 +0330

    add Initialized listener for getting data at startup

---
+++ src/main/java/InitializeListner.java
@@ -0,0 +1,38 @@
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.util.List;
+
+@WebListener
+public class InitializeListner implements ServletContextListener {
+    private static DataBase db = new DataBase();
+    private static Server server = new Server();
+    private static HttpClientGet http_client_get = new HttpClientGet();
+
+    @Override
+    public final void contextInitialized(final ServletContextEvent sce) {
+        try {
+            String project_data_json = http_client_get.HttpGetRequest(RequestType.PROJECT);
+            String skills_data_json = http_client_get.HttpGetRequest(RequestType.SKILL);
+            Skill.setValidNames(JsonParser.parseNameList(skills_data_json));
+            List<Project> projs = JsonParser.parseProjectList(project_data_json);
+            for(Project p : projs)
+                DataBase.addProject(p);
+            DataBase.setLoginUser(new User("1", "علی", "شریف زاده", "برنامه نویس وب",
+                    "",
+                    "روی سنگ قبرم بنویسید: خدا بیامرز می خواست خیلی کارا بکنه ولی پول نداشت",
+                    new Skill("HTML", 5), new Skill("Javascript", 4), new Skill("C++", 2),
+                    new Skill("Java", 3)));
+            System.out.println(project_data_json);
+        }
+        catch (IOException ioException) {
+            return;
+        }
+    }
+
+    @Override
+    public final void contextDestroyed(final ServletContextEvent sce) {
+
+    }
+}
