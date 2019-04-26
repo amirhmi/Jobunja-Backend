5cc133284c4439a3d710f85e846691e4efc4e6a0
amirhmi <amirhmi1377@gmail.com>
2019-04-26 12:02:48 +0430

    change database path to tomcat root

--- src/main/java/DataAccess/DBCPDataSource.java
+++ src/main/java/DataAccess/DBCPDataSource.java
@@ -2,6 +2,7 @@ package DataAccess;
 
 import org.apache.commons.dbcp.BasicDataSource;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -10,12 +11,13 @@ public class DBCPDataSource {
     private static BasicDataSource ds = new BasicDataSource();
 
     static {
+        File dbfile = new File(".");
         ds.setDriverClassName("org.sqlite.JDBC");
-        ds.setUrl("jdbc:sqlite:/Users/amirhmi/Desktop/jobunja.db");
+        System.out.println(dbfile.getAbsolutePath());
+        ds.setUrl("jdbc:sqlite:" + dbfile.getAbsolutePath() + "jobunja.db");
         ds.setMinIdle(5);
         ds.setMaxIdle(10);
         ds.setMaxOpenPreparedStatements(100);
-        System.out.println("database created");
     }
 
     public static Connection getConnection() throws SQLException {
