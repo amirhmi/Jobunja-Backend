424c8137326fb1cb7d9f69a8dddfddddcac783ca
iambb5445 <iambb5445@yahoo.com>
2019-04-27 07:57:20 +0430

    UserDataMapper.java added (not working when adding skills because of tomcat memory leak warning)

---
+++ src/main/java/DataAccess/UserDataMapper.java
@@ -0,0 +1,52 @@
+package DataAccess;
+
+import Model.Entity.Skill;
+import Model.Entity.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDataMapper {
+    public static User find(String userId) throws SQLException {
+        Connection db = DataSource.getConnection();
+        String statement = "SELECT * FROM user WHERE id = ?";
+        PreparedStatement dbStatement = db.prepareStatement(statement);
+        dbStatement.setString(1, userId);
+        ResultSet rs = dbStatement.executeQuery();
+        while (rs.next())
+            System.out.println(rs.getType());
+        //User user = new User(userId, );
+        return null;
+
+    }
+
+    public static void insert(User user) throws SQLException {
+        Connection db = DataSource.getConnection();
+        String statement =
+                "INSERT INTO User(id, firstName, lastName, jobTitle, profilePicUrl, bio)" +
+                        "VALUES(?, ?, ?, ?, ?, ?)";
+        PreparedStatement dbStatement = db.prepareStatement(statement);
+        dbStatement.setString(1, user.getId());
+        dbStatement.setString(2, user.getFirstName());
+        dbStatement.setString(3, user.getLastName());
+        dbStatement.setString(4, user.getJobTitle());
+        dbStatement.setString(5, user.getProfilePicUrl());
+        dbStatement.setString(6, user.getBio());
+        dbStatement.execute();
+        dbStatement.close();
+        db.close();
+        statement = "INSERT INTO UserSkill(userid, skillName)" +
+                            "VALUES(?, ?)";
+        for (Skill s : user.getSkills()) {
+            db = DataSource.getConnection();
+            dbStatement = db.prepareStatement(statement);
+            dbStatement.setString(1, user.getId());
+            dbStatement.setString(2, s.getName());
+            dbStatement.execute();
+            dbStatement.close();
+            db.close();
+        }
+    }
+}
--- src/main/java/Model/Service/InitializeListner.java
+++ src/main/java/Model/Service/InitializeListner.java
@@ -33,7 +33,7 @@ public class InitializeListner implements ServletContextListener {
             List<String> validSkillNames = JsonParser.parseNameList(skills_data_json);
             for(String skillName : validSkillNames)
                 SkillDataMapper.insert(skillName);
-            List<Project> projs = JsonParser.parseProjectList(project_data_json);
+            //List<Project> projs = JsonParser.parseProjectList(project_data_json);
 //            for(Project p : projs) {
 //                ProjectDataMapper.insert(p);
 //                for(Skill skill : p.getSkills()) {
@@ -43,17 +43,17 @@ public class InitializeListner implements ServletContextListener {
 //            }
 //                DataBase.addProject(p);
 
-            DataBase.setLoginUser(new User("1", "علی", "شریف زاده", "برنامه نویس وب",
+            UserDataMapper.insert(new User("1", "علی", "شریف زاده", "برنامه نویس وب",
                     "https://i.ibb.co/wCkChfK/ali.jpg",
                     "روی سنگ قبرم بنویسید: خدا بیامرز می خواست خیلی کارا بکنه ولی پول نداشت",
                     new Skill("HTML", 5), new Skill("Javascript", 4), new Skill("C++", 2),
                     new Skill("Java", 3)));
-            DataBase.addUser(new User("2", "امیرحسین", "احمدی", "دانش‌پژوه",
+            UserDataMapper.insert(new User("2", "امیرحسین", "احمدی", "دانش‌پژوه",
                     "https://i.ibb.co/5FRFzY8/amir.jpg",
                     "روی سنگ قبرم بنویسید: خدا بیامرز می خواست خیلی کارا بکنه ولی پول نداشت",
                     new Skill("HTML", 5), new Skill("Django", 6), new Skill("PHP", 2),
                     new Skill("Java", 3), new Skill("Node.js", 10)));
-            DataBase.addUser(new User("3", "بهار", "باطنی", "گیمر",
+            UserDataMapper.insert(new User("3", "بهار", "باطنی", "گیمر",
                     "https://i.ibb.co/d5Y4PDK/bahar.jpg",
                     "روی سنگ قبرم بنویسید: خدا بیامرز می خواست خیلی کارا بکنه ولی پول نداشت",
                     new Skill("C", 4), new Skill("C++", 7), new Skill("Photoshop", 1),
